5a5d51cabefabb63f737f1df214c347f4dbd4fdb
Gnieva94 <gus_94@hotmail.com.ar>
2023-08-16 22:54:25 -0300

    Add main structure; popup; image

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log('Hello World')
+const addTask = document.querySelector('#addTask')
+const inputTask = document.querySelector('#inputTask')
+const popUp = document.querySelector('#popUp')
+
+
+addTask.addEventListener('click',(e)=>{
+    e.preventDefault()
+    addTask.classList.toggle('d-none')
+    popUp.classList.toggle('d-none')
+    inputTask.focus()
+})
+popUp.addEventListener('click',(e)=>{
+    e.preventDefault()
+    if(!(e.target.id == 'taskHolder' || e.target.id == 'inputTask' || e.target.id == 'taskBtnSubmit')){
+        popUp.classList.toggle('d-none')
+        addTask.classList.toggle('d-none')
+    }
+})
+
+
+console.log(addTask)
